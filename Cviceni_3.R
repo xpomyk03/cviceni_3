cf99c81cf05aefbc53f1ae7f743ea1b0f72378cb
Barbora Pomykalova <xpomyk03@vutbr.cz>
2021-10-07 13:26:15 +0200

    Oprava funkce na vraceni minci.

--- Cviceni_3.R
+++ Cviceni_3.R
@@ -2,24 +2,6 @@
 
 # mince
 
-#VratitMince <- function(M){
-# Mince <- c(50, 20, 10, 5, 2, 1)
-# Vracene <- c()
-# 
-# if (M>0){
-#   for (i in 1:6){
-#     if (M >= Mince[i]){
-#       M = M - Mince[i]
-#       Vracene = c(Vracene, Mince[i])
-#       break
-#     }
-#   }
-#   VratitMince(M)
-# }else{
-#   return(Vracene)
-# }
-#}
-
 VratitMince <- function(vratna_castka){
   vratit <- c()
   mince <- c(50, 20, 10, 5, 2, 1)
@@ -30,6 +12,7 @@ VratitMince <- function(vratna_castka){
       if (x > 0){
         vratit <- c(vratit, mince[i])
         vratna_castka = vratna_castka - mince[i]
+        break
       }
     }
   }
